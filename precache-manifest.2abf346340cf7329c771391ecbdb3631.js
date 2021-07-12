696c0656585e06ff9fe4dd4204b0895926ecece8
Ethan Chen <ethanchen100598@gmail.com>
2021-07-12 17:39:03 -0400

    Updates

---
+++ precache-manifest.2abf346340cf7329c771391ecbdb3631.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "748ce93f61123abebcdbba3e3321913d",
+    "url": "/react-ant-init/index.html"
+  },
+  {
+    "revision": "b83f08cdb93d896e6bf4",
+    "url": "/react-ant-init/static/css/main.2b8ad8df.chunk.css"
+  },
+  {
+    "revision": "07853b32ec916fd4bab2",
+    "url": "/react-ant-init/static/js/2.105570fc.chunk.js"
+  },
+  {
+    "revision": "41e8aedee29e6e7a9a1bdd0a210ba283",
+    "url": "/react-ant-init/static/js/2.105570fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b83f08cdb93d896e6bf4",
+    "url": "/react-ant-init/static/js/main.b60f46be.chunk.js"
+  },
+  {
+    "revision": "1521d445284f98934a51",
+    "url": "/react-ant-init/static/js/runtime-main.09da17b8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Home/precache-manifest.9c7b8edb20729b5edd67bf54de066770.js"
+  "/react-ant-init/precache-manifest.2abf346340cf7329c771391ecbdb3631.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-ant-init/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
